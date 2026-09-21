568d0eca3c0a9b6acb605e659d43d7b83ed25299
Barts <32927243+SooLazy@users.noreply.github.com>
2026-09-21 13:58:29 +1000

    Update config_flow.py

--- custom_components/rust_rcon/config_flow.py
+++ custom_components/rust_rcon/config_flow.py
@@ -1,8 +1,8 @@
 """Config flow for Rust RCON."""
+from __future__ import annotations
+
 import logging
 _LOGGER = logging.getLogger(__name__)
-
-from __future__ import annotations
 
 from typing import Any
 
